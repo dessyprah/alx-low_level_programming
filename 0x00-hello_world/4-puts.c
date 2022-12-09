aca2f11671d161914e921d502e6058d2802cf875
dessyprah <infodessy21@gmy@gmail.com>
2022-12-08 19:56:15 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-/**
- * main -Entry
- * Return Always 0
+/*
+ * main - print a line of code using puts
+ *
+ * Return: 0
  */
 
 int main(void)
 
 {
-	put("\"Programming is like building a multi lingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
-
 }
