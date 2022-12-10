fe84a0ef28755681ddb6523bbf318960dbbd788c
dessyprah <infodessy21@gmy@gmail.com>
2022-12-09 20:44:10 -0800

    task 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
  * main - print a C program using printf
  *
  * return: 0
- */
+*/
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
-}
+       	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);   
+}        
