dbcb9c26bfcb2b494de74360f8abdc3f7ff8775d
dessyprah <infodessy21@gmy@gmail.com>
2022-12-12 22:44:03 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase, except for q and e.
+ *
+ * Returb: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
