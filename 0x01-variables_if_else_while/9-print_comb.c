4b202659dd5bd199d25826aa294454ba596f77ba
dessyprah <infodessy21@gmy@gmail.com>
2022-12-17 14:42:25 -0800

    task 100

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,9 +2,10 @@
 
 /**
  * main - Print all possible combinations of single-digit numbers.
- *
+ * Description: 'printing all possible combination of single-digit numbers'
  * Return; Always 0.
  */
+
 int main(void)
 {
 	int num;
